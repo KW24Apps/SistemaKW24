c30f2baa97eed02981183311b7f0666267c0931d
GabrielAcker <gabriel.acker@gmail.com>
2025-07-16 22:38:37 -0300

    atualizar servidor

--- public/login.php
+++ public/login.php
@@ -34,7 +34,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="../../assets/css/login.css">
 </head>
 <body>
     <?php if ($loginError): ?>
