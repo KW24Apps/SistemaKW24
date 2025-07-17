127335c599fa723d5180ae3113e16ddd366b25b5
GabrielAcker <gabriel.acker@gmail.com>
2025-07-17 09:11:32 -0300

    atualizar servidor

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,24 +1,27 @@
 <?php
 // public/dashboard.php
-?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="../assets/css/dashboard.css">
-</head>
-<body>
-    <?php include 'sidebar.php'; ?>
-    <div class="main-content">
-        <div class="dashboard-container">
-            <h1 id="dashboard-title">Dashboard</h1>
-            <div id="dashboard-date" class="dashboard-date"></div>
-            <button id="btn-refresh-dashboard">Atualizar</button>
-            <div id="dashboard-loader" class="dashboard-loader" style="display:none"></div>
-        </div>
-    </div>
-    <script src="../assets/js/dashboard.js"></script>
-</body>
-</html>
+session_start();
+require_once __DIR__ . '/../includes/helpers.php';
+requireAuthentication();
+
+$pageTitle = 'Dashboard - Sistema KW24';
+$activeMenu = 'dashboard';
+
+// Conteúdo do dashboard
+ob_start();
+?>
+<div class="dashboard-container">
+    <h1 id="dashboard-title">Dashboard</h1>
+    <div id="dashboard-date" class="dashboard-date"></div>
+    <button id="btn-refresh-dashboard">Atualizar</button>
+    <div id="dashboard-loader" class="dashboard-loader" style="display:none"></div>
+</div>
+<?php
+$content = ob_get_clean();
+
+$additionalCSS = '<link rel="stylesheet" href="/Apps/assets/css/dashboard.css">';
+$additionalJS  = '<script src="/Apps/assets/js/dashboard.js"></script>';
+
+// Layout base (sidebar, etc)
+include __DIR__ . '/../views/layouts/main.php';
