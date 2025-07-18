c7f3808a8b5aa7b7693bcb84692ac66ba7da8748
GabrielAcker <gabriel.acker@gmail.com>
2025-07-18 17:21:34 -0300

    atualizar servidor

--- public/login.php
+++ public/login.php
@@ -68,11 +68,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form method="post" action="login.php" autocomplete="off">
             <div class="input-group">
                 <span class="input-icon"><i class="fa fa-user"></i></span>
-                <input type="text" name="usuario" id="usuario" placeholder="Usuário" required>
+                <input type="text" name="usuario" id="usuario" placeholder="Usuário" required autocomplete="username" value="<?php echo isset($_POST['usuario']) ? htmlspecialchars($_POST['usuario']) : ''; ?>">
             </div>
             <div class="input-group" style="position:relative;">
                 <span class="input-icon"><i class="fa fa-lock"></i></span>
-                <input type="password" name="senha" id="senha" placeholder="Senha" required>
+                <input type="password" name="senha" id="senha" placeholder="Senha" required value="<?php echo isset($_POST['senha']) ? htmlspecialchars($_POST['senha']) : ''; ?>">
                 <span id="toggleSenha" style="position:absolute; right:16px; top:12px; cursor:pointer;">
                     <i class="fa fa-eye"></i>
                 </span>
