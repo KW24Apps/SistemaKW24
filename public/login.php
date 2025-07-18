ae1e79953bf23b208fad1e13f7f90242c4c8f805
GabrielAcker <gabriel.acker@gmail.com>
2025-07-18 10:05:56 -0300

    atualizar servidor

--- public/login.php
+++ public/login.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
+<h1 style="color: red; text-align: center;">TESTE LOGIN JURANDIR</h1>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <title>Login KW24</title>
-
+    <link rel="stylesheet" href="/Apps/assets/css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body>
