f823bac01736ffe63b893d6cb2a0a3e4bfabbd36
GabrielAcker <gabriel.acker@gmail.com>
2025-07-19 02:19:20 -0300

    atualizar servidor

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -1,17 +1,20 @@
 <div class="sidebar" id="sidebar">
-    <div class="sidebar-header">
-        <button id="sidebarToggle" class="sidebar-toggle" title="Expandir/Recolher Menu">
-            <i class="fas fa-bars"></i>
-            <span class="sidebar-expand-label">Expandir Menu</span>
-        </button>
-        <div class="sidebar-logo-text">
-            <img src="/Apps/assets/img/Logo_Menu.png" alt="Logo KW24" class="sidebar-logo-icon">
+    <div class="sidebar-section-wrapper">
+        <div class="sidebar-header">
+            <button id="sidebarToggle" class="sidebar-toggle" title="Expandir/Recolher Menu">
+                <i class="fas fa-bars"></i>
+                <span class="sidebar-expand-label">Expandir Menu</span>
+            </button>
+            <div class="sidebar-logo-text">
+                <img src="/Apps/assets/img/Logo_Menu.png" alt="Logo KW24" class="sidebar-logo-icon">
+            </div>
         </div>
     </div>
 <div class="sidebar-separator"></div>
     <div class="sidebar-logo">
 </div>
-    <ul class="sidebar-menu">
+    <div class="sidebar-section-wrapper">
+        <ul class="sidebar-menu">
         <li>
             <a href="#" class="sidebar-link" title="Dashboard">
                 <span class="sidebar-link-inner">
@@ -44,7 +47,8 @@
                 </span>
             </a>
         </li>
-    </ul>
+        </ul>
+    </div>
     <div class="sidebar-bottom">
         <a href="#" class="sidebar-link" title="Configurações">
             <span class="sidebar-link-inner">
