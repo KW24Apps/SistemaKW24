41b127de4a7944b021c42aa413d27762c5e1e5a1
GabrielAcker <gabriel.acker@gmail.com>
2025-07-21 20:35:09 -0300

    atualizar servidor

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-// public/dashboard.php (limpo)
+// public/dashboard.php (limpo e corrigido)
 
 session_start();
 require_once __DIR__ . '/../includes/helpers.php';
@@ -20,24 +20,6 @@ $content = ob_get_clean();
 
 $additionalCSS = '<link rel="stylesheet" href="/Apps/assets/css/area-atuacao.css">';
 $additionalJS  = '';
-
-// Layout base (sidebar, etc)
-include __DIR__ . '/../views/layouts/main.php';
-?>
-    </div>
-    <!-- TESTE: Visualize a responsividade e alinhamento da área branca -->
-    <div class="atuacao-teste-info">
-      <p>Esta área branca deve estar centralizada e responsiva.<br>
-      Altere o estado da sidebar (minimizada/maximizada) para testar o alinhamento.<br>
-      <span style="font-size:0.95em;color:#888;">(Remova este bloco após validar o layout)</span></p>
-    </div>
-  </div>
-</div>
-<?php
-$content = ob_get_clean();
-
-$additionalCSS = '<link rel="stylesheet" href="/Apps/assets/css/dashboard.css">\n<link rel="stylesheet" href="/Apps/assets/css/area-atuacao.css">';
-$additionalJS  = '<script src="/Apps/assets/js/dashboard.js"></script>';
 
 // Layout base (sidebar, etc)
 include __DIR__ . '/../views/layouts/main.php';
