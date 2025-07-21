5d8700d925d88823b89d4d2da8979df85ad25e65
GabrielAcker <gabriel.acker@gmail.com>
2025-07-21 20:45:57 -0300

    atualizar servidor

---
+++ views/layouts/area-atuacao.php
@@ -0,0 +1,9 @@
+<?php
+// views/layouts/area-atuacao.php
+// Uso: include este arquivo e defina $areaContent antes de incluir
+?>
+<div class="area-atuacao-wrapper">
+  <div class="area-atuacao">
+    <?php if (isset($areaContent)) { echo $areaContent; } ?>
+  </div>
+</div>
