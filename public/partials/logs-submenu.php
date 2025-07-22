25a0db4eaeae6fda415573a1ddf8531700f56dde
GabrielAcker <gabriel.acker@gmail.com>
2025-07-22 14:23:59 -0300

    atualizar servidor

--- public/partials/logs-submenu.php
+++ public/partials/logs-submenu.php
@@ -2,3 +2,4 @@
     <button class="logs-submenu-btn" data-page="filtro">Filtro</button>
     <button class="logs-submenu-btn" data-page="download">Download</button>
 </div>
+<div class="logs-submenu-separator"></div>
