71b182442dc99ad7d959e114551ff906eadb8aa0
GabrielAcker <gabriel.acker@gmail.com>
2025-07-22 00:36:09 -0300

    atualizar servidor

--- public/dashboard.php
+++ public/dashboard.php
@@ -9,18 +9,17 @@ $pageTitle = 'Dashboard - Sistema KW24';
 $activeMenu = 'dashboard';
 
 
-
 ob_start();
 ?>
-<div style="text-align:center; color:#2a4a5a; font-size:1.1em;">
-  Área branca centralizada para teste.<br>
-  Altere o estado da sidebar (minimizada/maximizada) para validar o alinhamento.
+<div class="area-atuacao-wrapper">
+  <div class="area-atuacao">
+    <div style="text-align:center; color:#2a4a5a; font-size:1.1em;">
+      Área branca centralizada para teste.<br>
+      Altere o estado da sidebar (minimizada/maximizada) para validar o alinhamento.
+    </div>
+  </div>
 </div>
 <?php
-$areaContent = ob_get_clean();
-
-ob_start();
-include __DIR__ . '/../views/layouts/area-atuacao.php';
 $content = ob_get_clean();
 
 $additionalCSS = '<link rel="stylesheet" href="/Apps/assets/css/area-atuacao.css">';
