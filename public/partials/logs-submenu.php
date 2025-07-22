9ce3f95a98c073328f4a473db71226baf734ee58
GabrielAcker <gabriel.acker@gmail.com>
2025-07-22 14:31:33 -0300

    atualizar servidor

--- public/partials/logs-submenu.php
+++ public/partials/logs-submenu.php
@@ -1,5 +1,8 @@
+<?php
+$sub = isset($_GET['sub']) ? $_GET['sub'] : 'filtro';
+?>
 <div class="logs-submenu">
-    <button class="logs-submenu-btn" data-page="filtro">Filtro</button>
-    <button class="logs-submenu-btn" data-page="download">Download</button>
+    <button class="logs-submenu-btn<?php echo ($sub === 'filtro') ? ' active' : ''; ?>" data-page="filtro">Filtro</button>
+    <button class="logs-submenu-btn<?php echo ($sub === 'download') ? ' active' : ''; ?>" data-page="download">Download</button>
 </div>
 <div class="logs-submenu-separator"></div>
