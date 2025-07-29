d89b2b1781655fba85b1df331ba6ccc6a80cda47
GabrielAcker <gabriel.acker@gmail.com>
2025-07-29 01:13:23 -0300

    atualizar Gabriel

--- dao/DAO.php
+++ dao/DAO.php
@@ -3,14 +3,15 @@
 require_once __DIR__ . '/../helpers/Database.php';
 
 class DAO {
-    // Permite acesso ao PDO para consultas externas
-    public function getPdo() {
-        return $this->db;
-    }
     protected $db;
 
     public function __construct() {
         $this->db = (new Database())->getConnection();
+    }
+
+    // Permite acesso ao PDO para consultas externas
+    public function getPdo() {
+        return $this->db;
     }
 
     // Exemplo: buscar todos os registros de uma tabela
