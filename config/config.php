f60eb41a1d55f3b06e578d89876c42013077cdb9
Gabriel Acker <gabriel.acker@gmail.com>
2026-05-29 01:05:36 -0300

    OK 29/05/2026

--- config/config.php
+++ config/config.php
@@ -19,7 +19,7 @@ $configs = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
-        ]
+        ] 
     ],
     'producao' => [
         'driver'   => 'pgsql',
